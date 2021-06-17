a185cf6d4a8bfd3accda922017660d83d8b85371
Milan Felix Šulc <rkfelix@gmail.com>
2021-06-17 23:11:32 +0200

    Docker: improve entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -Eeo pipefail
 
 echo "EXPOSE_PORT=${EXPOSE_PORT}"
 echo "EXPOSE_HOST=${EXPOSE_HOST}"
@@ -7,5 +8,13 @@ echo "EXPOSE_PASSWORD=${EXPOSE_PASSWORD}"
 echo "EXPOSE_TOKEN=${EXPOSE_TOKEN}"
 echo "EXPOSE_ADMIN=${EXPOSE_ADMIN}"
 echo "EXPOSE_DB=${EXPOSE_DB}"
+printf "\n\n\n"
 
-/srv/expose serve ${EXPOSE_DOMAIN} --port ${EXPOSE_PORT}
+if [ -z "$1" ]
+then
+    echo "Expose: serve ${EXPOSE_HOST} --port ${EXPOSE_PORT}"
+    /srv/expose serve ${EXPOSE_HOST} --port ${EXPOSE_PORT} -vvv
+else
+    echo "Expose: $@"
+    /srv/expose $@
+fi
